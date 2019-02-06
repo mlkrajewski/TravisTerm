b7b6cddba4071456811990a2c0e5162d56caa7de
mlkrajewski <miko.krajewski@gmail.com>
2019-02-06 10:51:22 -0800

    first commit

---
+++ src/multiply.java
@@ -0,0 +1,8 @@
+
+public class multiply {
+	public static void main(String[] args) {
+		int i = 5, j = 6;
+		//Prints the multiplication of two integers
+		System.out.println("The product of " + i + " and " + j + " is " + (i*j));
+	}
+}
